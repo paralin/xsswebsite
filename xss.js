d1957a5afebf56f339e57f6608722e2cde6070e7
Christian Stewart <kidovate@gmail.com>
2015-07-10 19:32:16 -0700

    Fix

--- xss.js
+++ xss.js
@@ -17,6 +17,8 @@ var prof = "<aside class=profile-card><header><a target=_blank href=http://paral
     link.media = 'all';
     head.appendChild(link);
 	
+	var bod  = document.getElementsByTagName('body')[0];
+	
 	// Set the content of the page
 	bod.innerHTML = "<canvas id=c style=\"position:fixed;top:0;margin:0 auto;left:0;right:0;z-index:-3;\"></canvas>"+prof;
 }
